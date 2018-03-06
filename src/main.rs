b445691806f3259297f5946043e63386569a2be9
s.matthew.english <s.matthew.english@gmail.com>
2018-03-06 11:24:03 -0500

    compiles clean

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-extern crate num;
-use num::Complex;
+use std::str::FromStr;
 
-#[allow(dead_code)]
-fn complex_square_add_loop(c: Complex<f64>) {
-  let mut z = Complex { re: 0.0, im: 0.0};
-  loop {
-    z = z * z + c;
+/// Parse the string 's' as a coordinate pair, like `"400x600"` or `"1.0,0.5"`.
+///
+/// Specifically, 's' should have the form <left><sep><right>, where <sep> is
+/// the character given by the `separator` argument, and <left> and <right> are both
+/// strings that can be parsed by `T::from_str`.
+/// 
+/// If `s` has the proper form, return `Some<(x, y)>`. If it doesn't parse
+/// correctly, return `None`.
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+  match s.find(separator) {
+    None => None, 
+    Some(index) => {
+      match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+        (Ok(l), Ok(r)) => Some((l, r)),
+        _ => None
+      }
+    }
   }
 }
 
 fn main() {
     println!("Hello, world!");
+
+    parse_pair::<i32>("10,20",   ',');
 }
 
 
-use std::str::FromStr;
-
-/// Parse the string 's' as a coordinate pair, like `"400x600"` or `"1.0,0.5"`.
+#[test]
+fn test_parse_pair() {
+  assert_eq!(parse_pair::<i32>("",        ','), None);
+  assert_eq!(parse_pair::<i32>("10,",     ','), None);
+  assert_eq!(parse_pair::<i32>(",10",     ','), None);
+  assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+  assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+  assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
+  assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
